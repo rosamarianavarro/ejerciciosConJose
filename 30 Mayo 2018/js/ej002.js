328dc0c064e3089e0d67669ad985d05248929e8e
Unknown <rosy201080@hotmail.com>
2018-06-01 22:41:34 +0200

    Terminado ejercicios

---
+++ 30 Mayo 2018/js/ej002.js
@@ -0,0 +1,52 @@
+$('document').ready(function(){
+
+    //inicializar la variable de control:
+    let ultimaVez = 'O';
+
+    /* Cuando usuario hace click en
+       una de las cajas, se ejecuta la
+       sgt fct anonima
+    */
+    $('.caja').click( function() {
+
+            //solo si la caja en que hemos
+            //dado click está vacía:
+            
+            if( $(this).html() == "" ){
+
+                if(ultimaVez == 'X'){
+                    $(this).html('O');
+                    ultimaVez = 'O';
+                }else{
+                    $(this).html('X');
+                    ultimaVez = 'X';
+                }//if
+
+            }//if caja vacía 
+
+        }// fct anónima 
+    );//caja click
+
+
+
+    $('#img1').on('mouseover',function(){
+
+        let src1 = $('#img1').attr('src');
+        let src2 = $('#img2').attr('src');
+
+        $('#img1').attr('src', src2);
+        $('#img2').attr('src', src1);
+
+    });//on mouseover sobre img1
+
+    $('#img2').on('mouseover',function(){
+
+        let src1 = $('#img1').attr('src');
+        let src2 = $('#img2').attr('src');
+
+        $('#img2').attr('src', src1);
+        $('#img1').attr('src', src2);
+
+    });//on mouseover sobre img2
+
+});//document ready
